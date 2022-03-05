26f5b12b0fdd05cb1ec480d91376e9c5ae1d9602
patrickluvsoj <patricklovesoj@gmail.com>
2022-03-05 13:05:26 -0800

    initial push to github repo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send({hi: 'updated message'});
-});
+});full
 
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`)
