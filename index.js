3fdb4cc82947079ea153449d079c899e1d42db33
patrickluvsoj <patricklovesoj@gmail.com>
2022-03-05 12:58:58 -0800

    changed message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({hi: 'updated message'});
 });
 
 app.listen(PORT, () => {
