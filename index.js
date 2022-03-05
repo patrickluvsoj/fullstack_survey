41188cb9ded48df3adc4f9367081c54c579f58b4
patrickluvsoj <patricklovesoj@gmail.com>
2022-03-05 12:47:13 -0800

    initial deployment

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+app.listen(PORT, () => {
+    console.log(`listening to port ${PORT}`)
+});
