cd1a9736836c610424bc6adb6486ac48db2e2f0c
patrickluvsoj <patricklovesoj@gmail.com>
2022-03-06 10:40:57 -0800

    adding OAuth flow

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
+
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        //redirect url
+        callbackURL: '/auth/google/callback'
+        }, 
+        (accessToken) => {
+            console.log(accessToken);
+        }
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        //requesting specific permissions from google
+        scope: ['profile', 'email']
+    })
+);
+
+
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.send({hi: 'updated message'});
-});
-
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`)
 });
